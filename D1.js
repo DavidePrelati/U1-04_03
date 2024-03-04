55c703f8b366f3b3650b6a210dad45409965bb6a
Davide <davide.prelati@gmail.com>
2024-03-04 13:39:33 +0100

    first commit

---
+++ D1.js
@@ -0,0 +1,65 @@
+/* REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let number = 5 //indica una variabile NUMERICA
+let string = "Paolo" //indica una variabile STRINGA, cioè che contiene "testo"
+let bool = true //indica una variabile BOOLEANA, può essere vera o falsa
+let dato; //indica un tipo di dato INDEFINITO, non viene definito con un valore
+let dato2 = null //indica un tipo di dato NULLO
+const qualcosa = "Davide" //indica un tipo di dato CONSTANTE, non può variare nel tempo
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let myName = "Davide"
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let num1 = 12
+let num2 = 20
+let sum = num1 + num2
+console.log("numero 1:" + num1 + " + " + "numero 2:" + num2 + " = " + sum)
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+myName = "Prelati"
+//const myName="Davide" significa che non può essere modificata, perciò se provassi a modificarla mi darebbe
+//errore in console
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let y = 4
+let sottrazione = y - x
+console.log("y:" + y + " - " + "x:" + x + " = " + sottrazione)
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = "jhon"
+let name2 = "Jhon"
+console.log(name1 !== name2)
